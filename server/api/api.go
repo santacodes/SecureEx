9a8dcfc5360b188bc1f07d85b790d483200f90bf
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-03-25 09:37:46 +0530

    added gofiber

--- server/api/api.go
+++ server/api/api.go
@@ -8,9 +8,10 @@ import (
 
 func GetInfo(domain string) {
 
-	url := fmt.Sprintf("https://api.ip2whois.com/v2?key=15EDAD6CFD6CC07185515EDD2364FABC&domain=%s", domain)
-	fmt.Println(url)
-
+	url := ("https://api.ip2whois.com/v2?key=15EDAD6CFD6CC07185515EDD2364FABC&domain=" + domain)
+	fmt.Println("Your Domain is ", domain)
+	fmt.Println("\n")
+	fmt.Println("\n")
 	req, _ := http.NewRequest("GET", url, nil)
 
 	res, _ := http.DefaultClient.Do(req)
--- server/main.go
+++ server/main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"fmt"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/santacodes/SecureEx/server/api"
 )
 
 func main() {
+	fmt.Println("\n")
+	fmt.Println("------ SECUREX --------")
+	fmt.Println("\n")
+	fmt.Println("Fetching results from API")
+	fmt.Println("\n")
+	//pass the domain here
+	api.GetInfo("gsocorganizations.dev")
+	fmt.Println("\n")
+	fmt.Println("Hosting the Server!")
+	//server init
+	app := fiber.New()
 
-	fmt.Print("------ SECUREX --------")
-	api.GetInfo("www.google.com")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 
 }
