4002220d215f2aa5cdb8a16d3df4dcc9d490495a
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-03-26 03:06:48 +0530

    added SSL verification

--- server/api/stats/stats.go
+++ server/api/stats/stats.go
@@ -1,6 +1,7 @@
 package stats
 
 import (
+	"crypto/tls"
 	"fmt"
 	"strings"
 )
@@ -26,6 +27,20 @@ func Calc(domain string, age int) float64 {
 	//Get all the values in boolean and multiply them with the weights and add the results to get the predicted value
 
 	var v1 = new(vulnerability) //init a node
+
+	conn, err := tls.Dial("tcp", "blog.umesh.wtf:443", nil)
+	if err != nil {
+		panic("Server doesn't support SSL certificate err: " + err.Error())
+	} else {
+		fmt.Println("Host has SSL Certificate")
+	}
+
+	err = conn.VerifyHostname("blog.umesh.wtf")
+	if err != nil {
+		panic("Hostname doesn't match with certificate: " + err.Error())
+	} else {
+		fmt.Println("Hosts name matches with SSL ")
+	}
 
 	if strings.Contains(domain, "https") {
 		v1.Domain = 0 * (node.Domain)
